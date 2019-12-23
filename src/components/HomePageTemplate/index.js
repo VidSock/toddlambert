2394e89814a40735c4062bcf701234cd6dcc428d
twilightscapes <ihatespam@gmail.com>
2019-12-23 15:18:06 -0600

    home page revision 2

--- src/components/HomePageTemplate/index.js
+++ src/components/HomePageTemplate/index.js
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types'
 import Image from '../Image';
 import { graphql } from 'gatsby'
 import { HTMLContent } from '../Content'
-import ArticleTemplate from '../ArticleTemplate'
+// import ArticleTemplate from '../ArticleTemplate'
 // import { Link } from 'gatsby'
 import styled from "styled-components"
 
@@ -215,7 +215,7 @@ const HomePageTemplate = ({
 
 {description}
 
-<ArticleTemplate />
+
 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p> 
 </div>
 
